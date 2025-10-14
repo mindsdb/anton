1740898f528a5dc32a4d19835e6b19a461ad35ea
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-10-14 20:04:38 +0530

    added revision for updating types for min value and max value

---
+++ alembic/versions/006_fix_min_max_val_types.py
@@ -0,0 +1,32 @@
+"""fix_min_max_val_types
+
+Revision ID: 006_fix_min_max_val_types
+Revises: 005_add_description_to_ds
+Create Date: 2025-10-14 19:46:25.483496
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '006_fix_min_max_val_types'
+down_revision: Union[str, None] = '005_add_description_to_ds'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    with op.batch_alter_table('column_statistics') as batch_op:
+        batch_op.alter_column('min_value', type_=sa.Text(), existing_type=sa.String(length=255), nullable=True)
+        batch_op.alter_column('max_value', type_=sa.Text(), existing_type=sa.String(length=255), nullable=True)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    with op.batch_alter_table('column_statistics') as batch_op:
+        batch_op.alter_column('min_value', type_=sa.String(length=255), existing_type=sa.Text(), nullable=True)
+        batch_op.alter_column('max_value', type_=sa.String(length=255), existing_type=sa.Text(), nullable=True)
