eb80e442221b3093de6e091d798a184473ef86a4
Lucas Koontz <lucas.emanuel.koontz@gmail.com>
2025-10-21 08:08:25 -0700

    Updates revision id in alembic migration.
    Updates the revision id in the alembic migration script to be more descriptive.
    This change improves the traceability and identification of this specific migration.

--- alembic/versions/007_change_tenant_id_to_uuid.py
+++ alembic/versions/007_change_tenant_id_to_uuid.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '1b265e0bc513'
+revision: str = '007_change_tenant_id_to_uuid'
 down_revision: Union[str, None] = '006_fix_min_max_val_types'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
