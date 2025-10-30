d4f391c9affb3d7afa64e7cf0d54b917386ea69d
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-10-30 22:39:41 +0530

    added method to get status from Prefect client

--- minds/client/prefect.py
+++ minds/client/prefect.py
@@ -11,6 +11,16 @@ class PrefectClient:
     Client for Prefect.
     """
 
+    async def get_flow_run_state(self, flow_run_id: str) -> states.State:
+        """
+        Get the state of a flow run.
+        """
+        logger.debug(f"Getting state for flow run {flow_run_id}")
+        async with get_client() as client:
+            state = await client.read_flow_run_states(flow_run_id=flow_run_id)
+        logger.debug(f"Flow run {flow_run_id} is in state {state}")
+        return state
+
     async def cancel_flow_run(self, flow_run_id: str) -> None:
         """
         Cancel a flow run.
