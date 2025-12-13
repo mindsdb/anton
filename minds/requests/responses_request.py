5ff71cc714654fc4423f35ba9bd04ca39b8a7c30
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-12-12 17:02:02 -0800

    added the schema for Responses requests

---
+++ minds/requests/responses_request.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+
+from minds.schemas.chat import Message
+
+
+class ResponsesRequest(BaseModel):
+    input: str | list[Message] | None = Field(
+        default=None,
+        description="Input for the responses request, either a string or a list of messages"
+    )
+    # TODO: The OpenAI API also supports a conversation object?
+    conversation: str | None = Field(
+        default=None,
+        description="Conversation ID for the responses request, if not provided, a new conversation will be created"
+    )
+    model: str = Field(description="Model name for the chat completion request")
+    stream: bool | None = Field(
+        default=False,
+        description="Whether the chat completion request is streaming or not",
+    )
+    # TODO: Metadata has not been added? Is it needed?
