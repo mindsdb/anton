407d2bf4b6586b03f09e0c8ff0f9a2c1606af817
ZoranPandovski <zoran.pandovski@gmail.com>
2026-04-09 12:55:34 +0200

    new memory

---
+++ anton/core/memory/__init__.py
@@ -0,0 +1,15 @@
+from anton.core.memory.base import HippocampusProtocol
+from anton.core.memory.hippocampus import Engram, Hippocampus
+from anton.core.memory.episodes import Episode, EpisodicMemory
+from anton.core.memory.consolidator import Consolidator
+from anton.core.memory.cortex import Cortex
+
+__all__ = [
+    "HippocampusProtocol",
+    "Engram",
+    "Hippocampus",
+    "Episode",
+    "EpisodicMemory",
+    "Consolidator",
+    "Cortex",
+]
