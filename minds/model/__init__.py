3a777597a20e09ed7b27512e3a3af8ff16e14ead
ZoranPandovski <zoran.pandovski@gmail.com>
2026-06-02 15:29:25 +0200

    Rmv data catalog

--- minds/model/__init__.py
+++ minds/model/__init__.py
@@ -1,39 +1,19 @@
 """
-Database models for the Minds application.
+Database models for the Minds API (inference-only).
 
 This package contains SQLModel definitions for all database entities.
 """
 
 from .base import BaseSQLModel
+from .chat_completion import ChatCompletion
 from .conversation import Conversation
-from .data_catalog import Column, ColumnStatistics, DataCatalog, ForeignKeyConstraint, PrimaryKeyConstraint, Table
-from .datasource import Datasource
-from .memory_rule import MemoryRule, RuleType
-from .memory_topic import MemoryTopic
 from .message import Message
-from .message_event import MessageEvent
 from .message_tracing import MessageTracing
-from .mind import Mind
-from .mind_datasource import MindDatasource
-from .mind_datasource_table import MindDatasourceTable
 
 __all__ = [
     "BaseSQLModel",
-    "Mind",
-    "Datasource",
+    "ChatCompletion",
     "Conversation",
     "Message",
-    "MessageEvent",
     "MessageTracing",
-    "MindDatasource",
-    "MindDatasourceTable",
-    "DataCatalog",
-    "Table",
-    "Column",
-    "ColumnStatistics",
-    "ForeignKeyConstraint",
-    "PrimaryKeyConstraint",
-    "MemoryRule",
-    "RuleType",
-    "MemoryTopic",
 ]
